3ea36c4533bfd7e683cafcde4b705fc2f401f223
jeffersonabreu8591-source <jeffersonabreu8591@gmail.com>
2025-12-04 19:11:18 -0300

    db connect

--- handcrafted-haven/app/lib/db.ts
+++ handcrafted-haven/app/lib/db.ts
@@ -3,7 +3,10 @@ import postgres from "postgres";
 const connectionString =
   process.env.POSTGRES_URL ||
   process.env.DATABASE_URL ||
-  process.env.POSTGRES_PRISMA_URL;
+  process.env.POSTGRES_PRISMA_URL ||
+  process.env.STORAGE_DB_POSTGRES_URL ||
+  process.env.STORAGE_DB_DATABASE_URL ||
+  process.env.STORAGE_DB_POSTGRES_URL_NON_POOLING;
 
 if (!connectionString) {
   throw new Error("❌ Nenhuma variável de conexão POSTGRES encontrada");
@@ -12,4 +15,5 @@ if (!connectionString) {
 export const sql = postgres(connectionString, {
   ssl: "require",
 });
+
 console.log("📌 USING DATABASE:", connectionString);
